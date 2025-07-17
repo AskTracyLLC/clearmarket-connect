62ee952772644704128ead3dea7503bc06ce4bb9
AskTracyLLC <tracy@asktracyllc.com>
2025-07-16 20:52:37 -0500

    Update VendorProfilePage.tsx
    add user doc

--- src/pages/VendorProfilePage.tsx
+++ src/pages/VendorProfilePage.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import VendorProfile from "@/components/VendorProfile";
+import UserDocuments from "@/components/profile/UserDocuments";
 
 const VendorProfilePage = () => {
   return (
