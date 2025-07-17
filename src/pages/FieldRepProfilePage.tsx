cb31e7a828b1ed4227e9e354f9a5dbb1de141a9f
AskTracyLLC <tracy@asktracyllc.com>
2025-07-16 20:52:12 -0500

    Update FieldRepProfilePage.tsx
    add userdoc

--- src/pages/FieldRepProfilePage.tsx
+++ src/pages/FieldRepProfilePage.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import FieldRepProfile from "@/components/FieldRepProfile";
+import UserDocuments from "@/components/profile/UserDocuments";
 
 const FieldRepProfilePage = () => {
   return (
@@ -8,6 +9,9 @@ const FieldRepProfilePage = () => {
       <Header />
       <main className="py-8">
         <FieldRepProfile />
+        <div className="mt-8">
+          <UserDocuments onDocumentAdded={(doc) => console.log('New document:', doc)} />
+        </div>
       </main>
       <Footer />
     </div>
